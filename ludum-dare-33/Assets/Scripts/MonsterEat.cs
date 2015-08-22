878a3e42932d037c27d40ac40360927cab7c4197
Dillon Shook <dshook@alumni.nmt.edu>
2015-08-22 11:25:07 -0400

    Monster eats

---
+++ ludum-dare-33/Assets/Scripts/MonsterEat.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterEat : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            anim.SetTrigger("Eating");
+            Destroy(other.gameObject);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
