88f293b0f3731aaa4628bbd75b4143d3f1b63f9f
Dillon Shook <dshook@alumni.nmt.edu>
2015-08-22 15:11:50 -0400

    Skiier spawner and rectangle drawer

---
+++ ludum-dare-33/Assets/Scripts/SkiSpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkiSpawner : MonoBehaviour
+{
+    RectTransform spawnArea;
+    public GameObject skiierPrefab;
+
+    public float spawnTime = 3f;
+    float timer = 0f;
+
+    void Start()
+    {
+
+        spawnArea = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > spawnTime)
+        {
+            Spawn();
+            timer = 0;
+        }
+    }
+
+    void Spawn()
+    {
+        Vector2 spawnpoint = new Vector2(
+            Random.Range(spawnArea.position.x + spawnArea.rect.xMin, spawnArea.position.x + spawnArea.rect.xMax),
+            Random.Range(spawnArea.position.y + spawnArea.rect.yMin, spawnArea.position.y + spawnArea.rect.yMax )
+        );
+
+        Instantiate(skiierPrefab, spawnpoint, Quaternion.identity);
+    }
+}
---
+++ ludum-dare-33/Assets/Scripts/Util/DrawEditorRectangle.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode()]
+public class DrawEditorRectangle : MonoBehaviour
+{
+    public Color color = Color.green;
+
+    private Vector2 topLeft;
+    private Vector2 topRight;
+    private Vector2 bottomLeft;
+    private Vector2 bottomRight;
+
+    void Update()
+    {
+        CalcPositons();
+        DrawBox();
+    }
+
+    void CalcPositons()
+    {
+        var rect = GetComponent<RectTransform>();
+
+        
+        topLeft.Set(rect.position.x + rect.rect.xMin, rect.position.y + rect.rect.yMax);
+        topRight.Set(rect.position.x + rect.rect.xMax, rect.position.y + rect.rect.yMax);
+        
+        bottomLeft.Set(rect.position.x + rect.rect.xMin, rect.position.y + rect.rect.yMin);
+        bottomRight.Set(rect.position.x + rect.rect.xMax, rect.position.y + rect.rect.yMin);
+    }
+
+    void DrawBox()
+    {
+        //if (Input.GetKey (KeyCode.S)) {
+        Debug.DrawLine(topLeft, topRight, color);
+        Debug.DrawLine(topRight, bottomRight, color);
+        Debug.DrawLine(bottomRight, bottomLeft, color);
+        Debug.DrawLine(bottomLeft, topLeft, color);
+        //}
+    }
+}
