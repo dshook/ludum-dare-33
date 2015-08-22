91e6ee43b01dec8270fa0cbd11e944e9a8314a5b
Dillon Shook <dshook@alumni.nmt.edu>
2015-08-22 16:43:58 -0400

    Start sign and game controls

---
+++ ludum-dare-33/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    bool paused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("f2"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+        if (Input.GetKeyDown("f3"))
+        {
+            paused = !paused;
+
+            if (paused)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
