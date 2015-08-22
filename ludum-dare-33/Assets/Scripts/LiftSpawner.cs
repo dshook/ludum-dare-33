662d6c82be5098dc48f8481857c4b1a6adabd51c
Dillon Shook <dshook@alumni.nmt.edu>
2015-08-22 18:39:07 -0400

    Lifts moving around, need to fix positions

---
+++ ludum-dare-33/Assets/Scripts/LiftSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LiftSpawner : MonoBehaviour
+{
+
+    public Vector2 emptyPosition;
+    public Vector2 fullPosition;
+
+    public GameObject emptyPrefab;
+    public GameObject fullPrefab;
+
+    public int number;
+    public float offset = 5f;
+
+    void Start()
+    {
+        for (int i = 0; i < number; i++)
+        {
+            emptyPosition.Set(emptyPosition.x, emptyPosition.y + offset);
+            fullPosition.Set(fullPosition.x, fullPosition.y + offset);
+
+            GameObject newEmpty = Instantiate(emptyPrefab, emptyPosition, Quaternion.identity) as GameObject;
+            GameObject newFull = Instantiate(fullPrefab, fullPosition, Quaternion.identity) as GameObject;
+
+            newEmpty.transform.parent = gameObject.transform.parent;
+            newFull.transform.parent = gameObject.transform.parent;
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ ludum-dare-33/Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+    public Vector2 direction;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition((Vector2)rb.position + direction * Time.fixedDeltaTime);
+    }
+}
