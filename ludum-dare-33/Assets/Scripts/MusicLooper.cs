5dc07f565710b36bd8f4cc943a474c00436710d8
Dillon Shook <dshook@alumni.nmt.edu>
2015-08-23 19:42:12 -0400

    Music & looper

---
+++ ludum-dare-33/Assets/Scripts/MusicLooper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Util;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicLooper : MonoBehaviour
+{
+    AudioSource source;
+    public float introTime = 10f;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.Play();
+    }
+
+    void Update()
+    {
+        if (FloatUtils.CloseEnough(source.time, source.clip.length)) {
+            source.time = introTime;
+        }
+    }
+}
